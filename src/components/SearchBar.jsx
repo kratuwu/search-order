eedff9adeb5f422dd73b0be2637e687e8752a9b4
kratuwu <kratuwu@gmail.com>
2025-03-03 09:34:08 +0700

    feat: set responsive for search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { useForm } from "react-hook-form"
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export const SearchBar = (props) => {
   const { register, handleSubmit } = useForm();
-  const today = new Date().toISOString().split('T')[0];
+  const today = new Date().toISOString().split("T")[0];
 
   const onSubmit = (data) => {
     props.onSearch(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="lg:flex mb-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="lg:flex self-center">
       <div className="flex items-center mr-4 mb-4">
-        <label htmlFor="period" className="mr-1">Period</label>
+        <label htmlFor="period" className="mr-1">
+          Period
+        </label>
         <select
           id="period"
-          {...register('period')}
-          style={{ height: '2.25rem' }} 
+          {...register("period")}
+          style={{ height: "2.25rem" }}
           defaultValue="transmission"
           className="border border-gray-300 rounded px-3 w-full lg:w-auto"
         >
@@ -24,42 +26,54 @@ export const SearchBar = (props) => {
         </select>
       </div>
       <div className="flex items-center mr-4 mb-4">
-        <label htmlFor="status" className="mr-1">Status</label>
+        <label htmlFor="status" className="mr-1">
+          Status
+        </label>
         <select
           id="status"
-          {...register('status')}
-          style={{ height: '2.25rem' }} 
+          {...register("status")}
+          style={{ height: "2.25rem" }}
           defaultValue="waiting"
           className="border border-gray-300 rounded px-3 w-full lg:w-auto"
         >
           <option value="waiting">Waiting</option>
         </select>
       </div>
-      <div className="flex items-center mr-4 mb-4">
-        <label htmlFor="fromDate" className="mr-1">From</label>
-        <input 
-          type="date" 
-          id="fromDate" 
-          {...register('fromDate')} 
-          defaultValue={today} 
-          placeholder="From Date" 
-          className="border border-gray-300 rounded px-3 py-1 w-full lg:w-auto"
-        />
+      <div className="flex flex-nowrap">
+        <div className="flex items-center mr-4 mb-4 grow">
+          <label htmlFor="fromDate" className="mr-1">
+            From
+          </label>
+          <input
+            type="date"
+            id="fromDate"
+            {...register("fromDate")}
+            defaultValue={today}
+            placeholder="From Date"
+            className="border border-gray-300 rounded px-3 py-1 w-full lg:w-auto"
+          />
+        </div>
+        <div className="flex items-center mr-4 mb-4 grow">
+          <label htmlFor="fromDate" className="mr-1">
+            To
+          </label>
+          <input
+            type="date"
+            id="toDate"
+            {...register("toDate")}
+            defaultValue={today}
+            placeholder="To Date"
+            className="border border-gray-300 rounded px-3 py-1 w-full lg:w-auto"
+          />{" "}
+        </div>
       </div>
-      <div className="flex items-center mr-4 mb-4">
-        <label htmlFor="fromDate" className="mr-1">To</label>
-        <input 
-          type="date" 
-          id="toDate" 
-          {...register('toDate')} 
-          defaultValue={today} 
-          placeholder="To Date" 
-          className="border border-gray-300 rounded px-3 py-1 w-full lg:w-auto"
-        />      </div>
-      <button 
-        type="submit" className="bg-blue-500 text-white px-6 py-1 rounded-full lg:ml-1 w-full lg:w-auto"
-        style={{ height: 'calc(2.25rem)' }} 
-      >Search</button>
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-6 py-1 rounded-full lg:ml-1 w-full lg:w-auto"
+        style={{ height: "calc(2.25rem)" }}
+      >
+        Search
+      </button>
     </form>
   );
-}
+};
